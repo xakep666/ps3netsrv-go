b782815e981b0bbdf32987dd3bddd554cbf39bef
Vladimir Stolyarov <xakep6666@gmail.com>
2021-01-31 13:22:28 +0300

    Refactor mount_linux.go

--- pkg/internal/testutil/mount_linux.go
+++ pkg/internal/testutil/mount_linux.go
@@ -12,15 +12,64 @@ const (
 	MultiExtentFileSupported = true
 )
 
-const loopControl = "/dev/loop-control"
+// IOCTL consts
+const (
+	loopSetFd      = 0x4C00
+	loopCtlGetFree = 0x4C82
+	loopClrFd      = 0x4C01
+)
 
-func ioctl(fd uintptr, req uint, arg uintptr) (err error) {
-	_, _, e1 := syscall.Syscall(syscall.SYS_IOCTL, fd, uintptr(req), arg)
-	if e1 != 0 {
-		return e1
+// syscalls will return an errno type (which implements error) for all calls,
+// including success (errno 0). We only care about non-zero errnos.
+func errnoIsErr(err error) error {
+	if err.(syscall.Errno) != 0 {
+		return err
 	}
+	return nil
+}
 
-	return
+// Given a handle to a Loopback device (such as /dev/loop0), and a handle
+// to the image to loop mount (such as a squashfs or ext4fs image), preform
+// the required call to loop the image to the provided block device.
+func loop(loopbackDevice, image *os.File) error {
+	_, _, err := syscall.Syscall(
+		syscall.SYS_IOCTL,
+		loopbackDevice.Fd(),
+		loopSetFd,
+		image.Fd(),
+	)
+	return errnoIsErr(err)
+}
+
+// Given a handle to the Loopback device (such as /dev/loop0), preform the
+// required call to the image to unloop the file.
+func unloop(loopbackDevice *os.File) error {
+	_, _, err := syscall.Syscall(syscall.SYS_IOCTL, loopbackDevice.Fd(), loopClrFd, 0)
+	return errnoIsErr(err)
+}
+
+// Get the next loopback device that isn't used. Under the hood this will ask
+// loop-control for the LOOP_CTL_GET_FREE value, and interpolate that into
+// the conventional GNU/Linux naming scheme for loopback devices, and os.Open
+// that path.
+func nextLoopDevice() (*os.File, error) {
+	loopInt, err := nextUnallocatedLoop()
+	if err != nil {
+		return nil, err
+	}
+	return os.Open(fmt.Sprintf("/dev/loop%d", loopInt))
+}
+
+// Return the integer of the next loopback device we can use by calling
+// loop-control with the LOOP_CTL_GET_FREE ioctl.
+func nextUnallocatedLoop() (int, error) {
+	fd, err := os.OpenFile("/dev/loop-control", os.O_RDONLY, 0644)
+	if err != nil {
+		return 0, err
+	}
+	defer fd.Close()
+	index, _, err := syscall.Syscall(syscall.SYS_IOCTL, fd.Fd(), loopCtlGetFree, 0)
+	return int(index), errnoIsErr(err)
 }
 
 func MountISO(isoPath string) (targetPath string, cleanup func() error, err error) {
@@ -36,69 +85,34 @@ func MountISO(isoPath string) (targetPath string, cleanup func() error, err erro
 
 	defer isoFile.Close()
 
-	loopDevNumber, err := freeLoopNr()
+	lo, err := nextLoopDevice()
 	if err != nil {
-		return "", nil, fmt.Errorf("loop find failed: %w", err)
+		return "", nil, err
 	}
 
-	loopPath := fmt.Sprintf("/dev/loop%d", loopDevNumber)
-
-	loopDev, err := os.OpenFile(loopPath, os.O_RDWR, os.ModeDevice)
-	if err != nil {
-		return "", nil, fmt.Errorf("loop open failed: %w", err)
+	if err := loop(lo, isoFile); err != nil {
+		lo.Close()
+		return "", nil, fmt.Errorf("loop failed: %w", err)
 	}
 
-	defer loopDev.Close()
-
-	const (
-		loopSetFD   = 0x4C00
-		loopClearFD = 0x4C01
-	)
-
-	if err := ioctl(loopDev.Fd(), loopSetFD, isoFile.Fd()); err != nil {
-		return "", nil, fmt.Errorf("loop setup failed: %w", err)
-	}
-
-	if err := syscall.Mount(loopPath, isoPath, "iso9660", syscall.MS_RDONLY, ""); err != nil {
-		return "", nil, fmt.Errorf("failed to mount loop device: %w", err)
+	if err := syscall.Mount(lo.Name(), tempDir, "iso9660", syscall.MS_RDONLY, ""); err != nil {
+		unloop(lo)
+		lo.Close()
+		return "", nil, fmt.Errorf("mount failed: %w", err)
 	}
 
 	return tempDir, func() error {
 		defer os.Remove(tempDir)
+		defer lo.Close()
 
-		if err := syscall.Unmount(loopPath, 0); err != nil {
+		if err := syscall.Unmount(targetPath, 0); err != nil {
 			return fmt.Errorf("loop unmount failed: %w", err)
 		}
 
-		loopDev, err := os.OpenFile(loopPath, os.O_RDWR, os.ModeDevice)
-		if err != nil {
-			return fmt.Errorf("loop open failed: %w", err)
-		}
-
-		defer loopDev.Close()
-
-		if err := ioctl(loopDev.Fd(), loopClearFD, 0); err != nil {
-			return fmt.Errorf("loop clear failed: %w", err)
+		if err := unloop(lo); err != nil {
+			return fmt.Errorf("unloop failed: %w", err)
 		}
 
 		return nil
 	}, nil
-}
-
-func freeLoopNr() (loopNr uintptr, err error) {
-	const loopCtlGetFree = 0x4C82
-
-	loopctl, err := os.OpenFile(loopControl, os.O_RDWR, os.ModeDevice)
-	if err != nil {
-		return 0, fmt.Errorf("open loop-control failed: %w", err)
-	}
-
-	defer loopctl.Close()
-
-	_, _, nr := syscall.Syscall(syscall.SYS_IOCTL, loopctl.Fd(), loopCtlGetFree, 0)
-	if nr == -1 {
-		return 0, fmt.Errorf("no free loop devices")
-	}
-
-	return uintptr(nr), nil
 }
